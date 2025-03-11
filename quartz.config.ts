aa594a820918d1b5db523fc0fcae0568ae9e302a
Carlos L Ramirez <carloslrm@gmail.com>
2025-03-11 16:53:36 -0600

    Quartz sync: Mar 11, 2025, 4:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "MIIT102 Fundamentos de Modelado y Simulación",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "es-GT",
+    baseUrl: "carloslramirez.com/MIIT102-fundamentos-modelado-simulacion/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
